da3c9053320721947f15796341b8eeb397cdb1d5
nicolecasares <nicolecasares@gmail.com>
2018-03-06 22:04:35 -0800

    changes flex box

---
+++ css/script copy.js
@@ -0,0 +1,45 @@
+// <!-- <script src="script.js" defer></script> -->
+// console.log('this is a test');
+
+
+
+document.querySelector('.portfolio-item').addEventListener('mouseenter', function(){
+	document.querySelector('.this-project').classList.toggle('name-of-project');
+	// console.log('this works');
+});
+
+document.querySelector('.portfolio-item').addEventListener('mouseleave', function(){
+	document.querySelector('.this-project').classList.toggle('name-of-project');
+	// console.log('this works');
+});
+
+document.querySelector('.waterfall').addEventListener('mouseenter', function(){
+	document.querySelector('.this-project-1').classList.toggle('name-of-project');
+	// console.log('this works');
+});
+
+document.querySelector('.waterfall').addEventListener('mouseleave', function(){
+	document.querySelector('.this-project-1').classList.toggle('name-of-project');
+	// console.log('this works');
+});
+
+document.querySelector('.nature').addEventListener('mouseenter', function(){
+	document.querySelector('.this-project-2').classList.toggle('name-of-project');
+	// console.log('this works');
+});
+
+document.querySelector('.nature').addEventListener('mouseleave', function(){
+	document.querySelector('.this-project-2').classList.toggle('name-of-project');
+	// console.log('this works');
+});
+
+
+document.querySelector('.forest').addEventListener('mouseenter', function(){
+	document.querySelector('.this-project-3').classList.toggle('name-of-project');
+	// console.log('this works');
+});
+
+document.querySelector('.forest').addEventListener('mouseleave', function(){
+	document.querySelector('.this-project-3').classList.toggle('name-of-project');
+	// console.log('this works');
+});
